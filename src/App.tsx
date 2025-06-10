02b8863251c165574cb42835cd17a6582a765854
Vansh Dhillon <vanshdhillon2000@gmail.com>
2025-06-10 21:50:14 +0530

    Updated package-lock.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { motion } from 'framer-motion';
+import AnimatedCursor from 'react-animated-cursor';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import MetaBalls from './components/MetaBalls';
@@ -13,6 +14,39 @@ function App() {
   return (
     <Router>
       <div className="min-h-screen bg-charcoal-800 font-poppins relative">
+        {/* Animated Cursor */}
+        <AnimatedCursor
+          innerSize={8}
+          outerSize={35}
+          color="249, 196, 22"
+          outerAlpha={0.3}
+          innerScale={0.7}
+          outerScale={2}
+          clickables={[
+            'a',
+            'input[type="text"]',
+            'input[type="email"]',
+            'input[type="number"]',
+            'input[type="submit"]',
+            'input[type="image"]',
+            'label[for]',
+            'select',
+            'textarea',
+            'button',
+            '.link',
+            '.cursor-pointer'
+          ]}
+          outerStyle={{
+            border: '2px solid #F9C416',
+            backgroundColor: 'rgba(249, 196, 22, 0.1)',
+            mixBlendMode: 'difference'
+          }}
+          innerStyle={{
+            backgroundColor: '#F9C416',
+            mixBlendMode: 'difference'
+          }}
+        />
+        
         {/* Enhanced MetaBalls Background Effect */}
         <MetaBalls
           color="#F9C416"
